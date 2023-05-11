bdd3453a632821f5e89161a73c524ec13d005c09
marcela <richard.marcela@yahoo.com.br>
2023-05-11 17:16:39 +0100

    unset almost done

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,31 +6,43 @@
 /*   By: mrichard <mrichard@student.42porto.pt>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 20:23:02 by mrichard          #+#    #+#             */
-/*   Updated: 2023/05/06 21:07:15 by mrichard         ###   ########.fr       */
+/*   Updated: 2023/05/11 16:24:51 by mrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/* static void delete(t_env *env, int i)
+static void    free_env_var(t_env *env)
 {
-    
-} */
+    free(env->str);
+    free(env);
+}
 
 int unset_env(t_commands *command)
 {
-    (void)command;
-    /* int i;
-    t_env *current;
+    t_env   *prev;
+    t_env   *curr;
 
-    i = 0;
-    current = command->env;
-    while (command->env)
+    // Caso a variável a ser removida seja a primeira da lista
+    if (!ft_strcmp(command->token->str, command->env->str))
     {
-        if (!ft_strcmp(command->token->str, current->str))
-            delete(command->env, i);
-        i++;
-        current = current->next;
-    }*/
+        curr = command->env;
+        command->env = curr->next;
+        free_env_var(curr);
+        return (0);
+    }
+    prev = command->env;
+    curr = prev->next;
+    while (curr)
+    {
+        if (!ft_strcmp(command->token->str, curr->str))
+        {
+            prev->next = curr->next;
+            free_env_var(curr);
+            return (0);
+        }
+        prev = curr;
+        curr = prev->next;
+    }
     return (1);
-} 
+}
